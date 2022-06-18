1f65626d7f2fd87ba2d1ca43703c6786885436d6
RyanSin <ryansin937@gmail.com>
2022-06-18 16:31:29 +0900

    feat: 인터셉터(Interceptor) 예시 프로젝트 완료

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -1,8 +1,11 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseInterceptors } from '@nestjs/common';
+import { LoggingInterceptor } from 'src/interceptors/logging.interceptor';
 import { BoardService } from './board.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('board')
+//특정 Controller에 Interceoptor 적용
+// @UseInterceptors(LoggingInterceptor)
 export class BoardController {
   constructor(private readonly boardService: BoardService) {}
 
@@ -27,6 +30,8 @@ export class BoardController {
    * @returns 게시글 정보 리스트
    */
   @Get('')
+  //특정 Controller Route에 Interceoptor 적용
+  // @UseInterceptors(LoggingInterceptor)
   getBoardList() {
     return this.boardService.getBoardList();
   }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggingInterceptor } from './interceptors/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //프로젝트 전체에 Interceptor 적용 방식
+  app.useGlobalInterceptors(new LoggingInterceptor());
+
   await app.listen(3000);
 }
 bootstrap();
